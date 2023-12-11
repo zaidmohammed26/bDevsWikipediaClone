07eebf4fbf28f117774821d3d2c5875eb2f457f6
Kamran Mohammed <kamranabdulbasith@gmail.com>
2023-12-11 20:22:36 +0530

    app.css to App.css

--- src/app.jsx
+++ src/app.jsx
@@ -1,16 +1,15 @@
-import React from "react"
-import "./app.css"
-import Body from "./components/body/body"
-import Footer from "./components/footer/footer"
+import React from "react";
+import "./App.css";
+import Body from "./components/body/body";
+import Footer from "./components/footer/footer";
 
-
-function App(){
-    return(
-        <div className="main-body">
-            <Body />
-            <Footer/>
-        </div>
-    )
+function App() {
+  return (
+    <div className="main-body">
+      <Body />
+      <Footer />
+    </div>
+  );
 }
 
-export default App
+export default App;
